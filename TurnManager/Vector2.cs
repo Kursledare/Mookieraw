9fea27d6e68443ea80f4dbca178fb56e870088fb
Marcus Lönneborg <marcuslonneborg@gmail.com>
2016-01-21 14:06:06 +0100

    Added operators and Distance method to Vector2

--- TurnManager/Vector2.cs
+++ TurnManager/Vector2.cs
@@ -11,6 +11,34 @@ namespace TurnManager
         }
         public int X { get; set; }
         public int Y { get; set; }
-
+        public static Vector2 operator +(Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.X + b.X, a.Y + b.Y);
+        }
+        public static Vector2 operator -(Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.X - b.X, a.Y - b.Y);
+        }
+        public static Vector2 operator -(Vector2 a)
+        {
+            return new Vector2(-a.X, -a.Y);
+        }
+        public static Vector2 operator -(Vector2 a, int b)
+        {
+            return new Vector2(a.X - b, a.Y - b);
+        }
+        public static Vector2 operator *(Vector2 a, int b)
+        {
+            return new Vector2(a.X * b, a.Y * b);
+        }
+        public static float Distance(Vector2 a, Vector2 b)
+        {
+            var dx = a.X - b.X;
+            var dy = a.Y - b.Y;
+​
+            var x2y2 = (dx * dx) + (dy * dy);
+​
+            return (float)Math.Sqrt(x2y2);
+        }
     }
 }
