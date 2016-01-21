b6a15565a54bb18366f80f25b3d2231d568d4dbc
morgan johansson <morgan__hurra@hotmail.com>
2016-01-21 15:44:23 +0100

    Added Dice project.

---
+++ Dices/All.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+namespace Dices
+{
+    public interface IDice
+    {
+        //Int32 Roll()
+        //{
+        //    Random rand1 = new Random();
+
+        //    int sum = rand1.Next(1, 7);
+        //    int count = 1;
+
+        //    {
+        //        sum = rand1.Next(1, 7);
+        //        count++;
+        //    }
+
+        //    return count;
+        //}
+
+        Int32 Sides
+        {
+            get;
+            set;
+        }
+
+        String Name
+        {
+            get;
+            set;
+        }
+        
+    }
+
+    public class Dice
+    {
+        public class D3 : IDice
+        {
+            public string Name
+            {
+                get { return Name; }
+                set { Name = "D3"; }
+            }
+
+            public int Sides
+            {
+                get { return Sides; }
+                set { Sides = 3; }
+            }
+
+            public int Roll()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class D4 : IDice
+        {
+            public string Name
+            {
+                get { return Name; }
+                set { Name = "D4"; }
+            }
+
+            public int Sides
+            {
+                get { return Sides; }
+                set { Sides = 4; }
+            }
+
+            public int Roll()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class D8 : IDice
+        {
+            public string Name
+            {
+                get { return Name; }
+                set { Name = "D8"; }
+            }
+
+            public int Sides
+            {
+                get { return Sides; }
+                set { Sides = 8; }
+            }
+
+            public int Roll()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class D10 : IDice
+        {
+            public string Name
+            {
+                get { return Name; }
+                set { Name = "D10"; }
+            }
+
+            public int Sides
+            {
+                get { return Sides; }
+                set { Sides = 10; }
+            }
+
+            public int Roll()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class D12 : IDice
+        {
+            public string Name
+            {
+                get { return Name; }
+                set { Name = "D12"; }
+            }
+
+            public int Sides
+            {
+                get { return Sides; }
+                set { Sides = 12; }
+            }
+
+            public int Roll()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class D20 : IDice
+        {
+            public string Name
+            {
+                get { return Name; }
+                set { Name = "D20"; }
+            }
+
+            public int Sides
+            {
+                get { return Sides; }
+                set { Sides = 20; }
+            }
+
+            public int Roll()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class D100 : IDice
+        {
+            public string Name
+            {
+                get { return Name; }
+                set { Name = "D100"; }
+            }
+
+            public int Sides
+            {
+                get { return Sides; }
+                set { Sides = 100; }
+            }
+
+            public int Roll()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+}
