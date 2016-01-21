caaacf26bafe91e3684b80ea2673727dcd532330
Marcus Lönneborg <marcuslonneborg@gmail.com>
2016-01-21 18:48:43 +0100

    Added TurnManagerTest

---
+++ TurnManagerTest/TurnManagerTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using TurnManager;
+using TurnManager.interfaces;
+using static TurnManager.TurnManager;
+
+namespace TurnManagerTest
+{
+    [TestFixture]
+    public class TurnManagerTest
+    {
+        private class DummyGameObject:IGameObject
+        {
+            public int Initiative { get; }
+            public bool IsActive { get; }
+            public Vector2 Position { get; }
+            public void Action()
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+        [Test]
+        public void CanRegisterGameObject()
+        {
+            var go = new DummyGameObject();
+            Assert.That(GameObjects.Count,Is.EqualTo(0));
+            Register(go);
+            Assert.That(GameObjects.Count,Is.EqualTo(1));
+            Assert.That(GameObjects.Contains(go),Is.EqualTo(true));
+        }
+    }
+}
