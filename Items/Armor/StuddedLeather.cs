e02a90a81a6f0c8fe58f8dcbbfaa9914f85af875
Jason Smith <mookierah@gmail.com>
2016-01-21 21:33:57 +0100

    Moved files into folders.

---
+++ Items/Armor/StuddedLeather.cs
@@ -0,0 +1,14 @@
+﻿using Items.Enums;
+
+namespace GameEquipment
+{
+    class StuddedLeather : Armor
+    {
+        public StuddedLeather()
+        {
+            base.Name = "Studded Leather";
+            base.ArmorType = ArmorTypes.Light;
+            base.ArmorValue = 3;
+        }
+    }
+}
