fa3e07994765c8002e4f4de5de2fea49f8e922ea
Christopher guest <chris@wizzerd.com>
2016-01-21 15:50:07 +0100

    Items With weapons

---
+++ Items/Interfaces/IWeapon.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjecktMookieraaw_Weapon
+{
+    public interface IWeapon
+    {
+        int Enchanment { get; set; }
+
+        int NumOfDise { get; set; }
+
+        string DiceZise { get; }
+
+        int DamageRoll();
+    }
+}
---
+++ Items/LongSword.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+namespace ProjecktMookieraaw_Weapon
+{
+    public class LongSword : IWeapon , IItems
+    {
+        public int Enchanment
+        {
+            
+            get
+            {
+                return Enchanment;
+            }
+
+            set
+            {
+                Enchanment = value;
+            }
+        }
+
+        public string Name { get; set; }
+
+        public int NumOfDise
+        {
+            get { return NumOfDise; }
+            set { NumOfDise = value; }
+        }
+
+        public string DiceZise
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int DamageRoll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
