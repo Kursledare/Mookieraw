c30caf415f33fef444442b79b0682242e8bf4c6e
Marcus Lönneborg <marcuslonneborg@gmail.com>
2016-01-23 18:01:55 +0100

    Added Test in TurnManagerTests

--- Tests/TurnManagerTests.cs
+++ Tests/TurnManagerTests.cs
@@ -1,19 +1,22 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using EntityData.Characters;
+using EntityData.Monsters;
 using NUnit.Framework;
 using TurnManager;
 using TurnManager.interfaces;
+using static TurnManager.TurnManager;
 
 namespace Tests
 {
     [TestFixture]
     public class TurnManagerTest
     {
-        private class DummyGameObject:IGameObject
+        private class DummyGameObject : IGameObject
         {
-            public Boolean ActionCalled { get; private set; }
+            public bool ActionCalled { get; private set; }
             public int Initiative { get; } = 1;
             public bool IsActive { get; } = true;
-            public Vector2 Position { get; set; }=new Vector2();
+            public Vector2 Position { get; set; } = new Vector2();
             public IGameObject Target { get; set; }
 
             public void Action()
@@ -23,28 +26,50 @@ namespace Tests
         }
 
         [Test]
-        public void CanRegisterGameObject()
-        {
-            var go = new DummyGameObject();
-            Assert.That(TurnManager.TurnManager.GameObjects.Count,Is.EqualTo(0));
-            TurnManager.TurnManager.Register(go);
-            Assert.That(TurnManager.TurnManager.GameObjects.Count,Is.EqualTo(1));
-            Assert.That(TurnManager.TurnManager.GameObjects.Contains(go),Is.EqualTo(true));
-            TurnManager.TurnManager.Unregister(go);
-            TurnManager.TurnManager.RunTurn();
-        }
-
-        [Test]
         public void ActionIsCalledOnGameObjectInRunTurn()
         {
             var go = new DummyGameObject();
-            TurnManager.TurnManager.Register(go);
-            Assert.That(go.ActionCalled,Is.False);
-            TurnManager.TurnManager.RunTurn();
-            Assert.That(go.ActionCalled,Is.True);
-            TurnManager.TurnManager.Unregister(go);
-            TurnManager.TurnManager.RunTurn();
+            Register(go);
+            Assert.That(go.ActionCalled, Is.False);
+            RunTurn();
+            Assert.That(go.ActionCalled, Is.True);
+            Unregister(go);
+            RunTurn();
+        }
 
+
+        [Test]
+        public void CanRegisterGameObject()
+        {
+            var go = new DummyGameObject();
+            Assert.That(GameObjects.Count, Is.EqualTo(0));
+            Register(go);
+            Assert.That(GameObjects.Count, Is.EqualTo(1));
+            Assert.That(GameObjects.Contains(go), Is.EqualTo(true));
+            Unregister(go);
+            RunTurn();
+        }
+        /// <summary>
+        /// Test that all Entitys can be register in TurnManager
+        /// If you add new please add them here..
+        /// </summary>
+        [Test]
+        public void CanRegisterRealStuffToTurnManager()
+        {
+            var listofObjectsToRegister = new List<object>
+            {
+                new BasicFighter("TestFighter"),
+                new Goblin()
+            };
+
+            listofObjectsToRegister.ForEach(x =>
+            {
+                Assert.That(x is IGameObject, Is.True);
+                var y = x as IGameObject;
+                Assert.DoesNotThrow(() => { Register(y); });
+            });
+            Assert.That(GameObjects.Count, Is.EqualTo(listofObjectsToRegister.Count));
+            listofObjectsToRegister.ForEach(x => { Unregister(x as IGameObject); });
         }
     }
 }
