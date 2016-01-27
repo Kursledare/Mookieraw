e931ee526c72ffac36c94ea16cf971be7b6004e2
TjackPounder <morgan.joh92@gmail.com>
2016-01-27 19:09:36 +0100

    Fixat i [Race.cs] och lagt till [Dwarf.cs]

---
+++ EntityData/Races/Dwarf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityData.Races
+{
+    public class Dwarf : Race
+    {
+        public Dwarf()
+        {
+            base.Size = Size.Medium;
+            base.Type = Type.Humanoid;
+            base.Subrace = SubRace.Dwarf;
+            base.BaseSpeed.Feet = 20;
+        }
+    }
+}
