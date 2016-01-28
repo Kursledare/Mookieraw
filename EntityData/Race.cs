612d2a7a5ecd6864159f6f251a2cc6ae22968678
TjackPounder <morgan.joh92@gmail.com>
2016-01-28 14:24:59 +0100

    Added [Gnome.cs] [Goblin.cs] [Halfling.cs] [Human.cs]

--- EntityData/Race.cs
+++ EntityData/Race.cs
@@ -9,6 +9,7 @@ namespace EntityData
         public Type Type { get; set; }
         public SubRace Subrace { get; set; }
         public BaseSpeed BaseSpeed { get; set; }
+        public bool CanSpeak;
     }
 
     public class BaseSpeed
@@ -27,6 +28,12 @@ namespace EntityData
     }
     public enum SubRace
     {
-        Human, Dwarf, Elf, Gnome, Halfling
+        Human, Dwarf, Elf, Gnome, Halfling, Goblin 
+    }
+
+    public enum Languages
+    {
+        Aboleth, Abyssal, Aklo, Aquan, Auran, Boggard, Celestial, Common, Cyclops, DarkFolk, Draconic, DrowSignLanguage, Druidic, Dwaren, Dziriak, Elven, Giant, Gnoll, Gnome, Goblin, Grippli, Halfling,
+        Ignan, Infernal, Necril, Orc, Protean, Sphinx, Sylvan, Tengu, Terran, Treant, Undercommon, Vegepygmy
     }
 }
---
+++ EntityData/Races/Gnome.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityData.Races
+{
+    public class Gnome : Race
+    {
+        public Gnome()
+        {
+            Size = Size.Small;
+            Type = Type.Humanoid;
+            Subrace = SubRace.Gnome;
+            BaseSpeed.Feet = 20;
+        }
+    }
+}
