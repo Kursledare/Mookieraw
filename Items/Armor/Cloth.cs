4ae17a1c71845440f64682746f503894ae7e5acf
victor ericsson <token_master@hotmail.com>
2016-01-29 10:48:56 +0100

    Just added cloth. Felt like it should be there for wizards and so on.

---
+++ Items/Armor/Cloth.cs
@@ -0,0 +1,14 @@
+﻿using Items.Enums;
+
+namespace Items.Armor
+{
+    class Cloth : GameEquipment.Armor
+    {
+        public Cloth()
+        {
+            Name = "Cloth Armor";
+            ArmorType = ArmorTypes.Light;
+            ArmorValue = 1;
+        }
+    }
+}
