84e7f5c1d494d410b2aff0d1056c21afdf39348c
Marcus Lönneborg <marcuslonneborg@gmail.com>
2016-02-02 19:17:35 +0100

    Added comments.

--- GameEngine/interfaces/IGameObject.cs
+++ GameEngine/interfaces/IGameObject.cs
@@ -1,13 +1,22 @@
 ﻿namespace GameEngine.interfaces
 {
+    /// <summary>
+    /// The interface used by GameManager to handle Objects that are in the game.
+    /// </summary>
     public interface IGameObject
     {
         int Initiative { get;  }
+        /// <summary>
+        /// Determines if Action is invoked on the object in RunTurn. Should be true when "Alive" and false when "Dead".
+        /// </summary>
         bool IsActive { get; }
         Vector2 Position { get; set; }
         ScreenObject ScreenObject { get; set; }
         Vector2 MovePosition { get; set; }
         IGameObject Target { get; set; }
+        /// <summary>
+        /// Invoked by GameManager in RunTurn. Needs to perform the Actions that the Object is going to do in the turn.
+        /// </summary>
         void Action();
     }
 }
