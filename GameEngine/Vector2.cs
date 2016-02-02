d95b97c716205d8644fd3ec648486aa935ccaf57
Marcus Lönneborg <marcuslonneborg@gmail.com>
2016-02-02 19:09:38 +0100

    Added comments.

--- GameEngine/Vector2.cs
+++ GameEngine/Vector2.cs
@@ -3,6 +3,9 @@ using System.Text;
 
 namespace GameEngine
 {
+    /// <summary>
+    /// Represents a point in 2 dimensions.
+    /// </summary>
     public class Vector2
     {
         public Vector2(float x = 0, float y = 0)
@@ -37,7 +40,12 @@ namespace GameEngine
         {
             return new Vector2(a.X*b, a.Y*b);
         }
-
+        /// <summary>
+        /// Calculate Distance between 2 Vector2
+        /// </summary>
+        /// <param name="a">First Vector2</param>
+        /// <param name="b">Second Vector2</param>
+        /// <returns>distance between the 2 Vector2</returns>
         public static float Distance(Vector2 a, Vector2 b)
         {
             var dx = a.X - b.X;
@@ -45,12 +53,21 @@ namespace GameEngine
             var x2Y2 = dx*dx + dy*dy;
             return (float) Math.Sqrt(x2Y2);
         }
-
+        /// <summary>
+        /// Calculate Distance from this Vector2 to another Vector2
+        /// </summary>
+        /// <param name="b">Second Vector2</param>
+        /// <returns>distance to the second Vector2</returns>
         public float Distance(Vector2 b)
         {
             return Distance(this, b);
         }
-
+        /// <summary>
+        /// Calculate Unit Vector based on the 2 Vector2(A Vector2 with magnitude of 1).
+        /// </summary>
+        /// <param name="a">First Vector2</param>
+        /// <param name="b">Second Vector2</param>
+        /// <returns>Unit Vector</returns>
         public static Vector2 Normalize(Vector2 a, Vector2 b)
         {
             var length = Distance(a, b);
@@ -58,6 +75,11 @@ namespace GameEngine
             var ny = (b.Y - a.Y)/length;
             return new Vector2(nx,ny);
         }
+        /// <summary>
+        /// Calculate Unit Vector from this Vector2 to Second Vector2(A Vector2 with magnitude of 1).
+        /// </summary>
+        /// <param name="b">Second Vector2</param>
+        /// <returns>Unit Vector</returns>
         public Vector2 Normalize(Vector2 b)
         {
             return Normalize(this, b);
