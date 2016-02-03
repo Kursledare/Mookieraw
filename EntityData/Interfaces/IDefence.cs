252628581e8e55c68146885e665c0399a26e6c8b
MookieRah <mookierah@gmail.com>
2016-02-03 10:23:39 +0100

    Added IDefence

---
+++ EntityData/Interfaces/IDefence.cs
@@ -0,0 +1,12 @@
+﻿namespace EntityData.Interfaces
+{
+    public interface IDefence
+    {
+        int Ac { get; }
+        int FlatFootedAc { get; }
+        int TouchAc { get; }
+        int FortitudeSave { get; }
+        int ReflexSave { get; }
+        int WillSave { get; }
+    }
+}
